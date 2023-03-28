239fd879675c556cd91269738fda97625d273cb2
fffsky <fff.238238238@gmail.com>
2023-03-28 18:07:35 +0900

    pop

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,24 @@
-window.onload = function() {
-    var popup = document.getElementById('js-popup');
-    if(!popup) return;
-    popup.classList.add('is-show');
-    
-    var blackBg = document.getElementById('js-black-bg');
-    var closeBtn = document.getElementById('js-close-btn');
-    
-    closePopUp(blackBg);
-    closePopUp(closeBtn);
-    
-    function closePopUp(elem) {
-    if(!elem) return;
-    elem.addEventListener('click', function() {
-    popup.classList.remove('is-show');
-    })
+window.addEventListener('load', function() {
+    if(!sessionStorage.getItem('disp_popup')) {
+        sessionStorage.setItem('disp_popup', 'on');
+        const body = document.querySelector('body');
+        const bgPopup = document.querySelector('.bg_onetime_popup');
+        const popup = document.querySelector('.onetime_popup');
+        const popupTitleClose = document.querySelector('.onetime_popup_title_close');
+        body.classList.add('open_popup');
+
+        bgPopup.addEventListener('click', function() {
+            closePopup();
+        });
+        popup.addEventListener('click', function(e) {
+            e.stopPropagation();
+        });
+        popupTitleClose.addEventListener('click', function() {
+            closePopup();
+        });
+
+        function closePopup() {
+            body.classList.remove('open_popup');
+        }
     }
-    }
+}, false);
