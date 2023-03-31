4bc257c5ec5be76ea032ee74c11f9e37c4c7cab8
fffsky <fff.238238238@gmail.com>
2023-03-31 15:43:22 +0900

    header

--- js/header.js
+++ js/header.js
@@ -1,30 +1,14 @@
-$(function() {
-  let pos = 0;
-  $(window).on('scroll', function () {
-    const $header = $('header');
-    const $main = $('main');
-    const headerHeight = $header.outerHeight();
-    const currentPos = $(this).scrollTop(); // スクロール量
-
-    // スクロール量がヘッダーの高さより大きい場合
-    if (currentPos > headerHeight) {
-      // headerを固定
-      $header.addClass('is-fixed');
-      $main.css('margin-top', 0);
+$(function () {
+  // スクロールを開始したら
+  $(window).on("scroll", function () {
+    // ファーストビューの高さを取得
+    mvHeight = $(".js-mv").height();
+    if ($(window).scrollTop() > mvHeight) {
+      // スクロールの位置がファーストビューより下の場合にclassを付与
+      $(".js-header").addClass("transform");
     } else {
-      // それ以外は固定を解除
-      $header.removeClass('is-fixed');
-      $main.css('margin-top', 0);
+      // スクロールの位置がファーストビューより上の場合にclassを外す
+      $(".js-header").removeClass("transform");
     }
-
-    // 上スクロール時
-    if (currentPos < pos) {
-      // header引っ込める
-      $header.css('top', 0);
-    } else {
-      //下スクロール時はheader出す
-      $header.css('top', -headerHeight);
-    }
-    pos = currentPos;
   });
 });
